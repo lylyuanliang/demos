28b7312d809f353156cfb40e91e6560c109661f4
liurl <lylyuanliang@163.com>
2023-08-11 13:38:11 +0800

    feat: 连续数组分割方法重载, 支持间隔自定义

--- common/src/main/java/com/example/common/util/ContinuousListUtils.java
+++ common/src/main/java/com/example/common/util/ContinuousListUtils.java
@@ -61,6 +61,19 @@ public class ContinuousListUtils {
     }
 
     /**
+     * 分割出连续数据作为一个新list
+     *
+     * @param sortedList  有序原始集合
+     * @param value2Int   将集合中的对象转成int, 做连续判断
+     * @param intervalFun 连续间隔
+     * @param <T>         目标值(原始值)
+     * @return
+     */
+    public static <T> List<List<T>> getContinuousLists(List<T> sortedList, ToIntFunction<T> value2Int, ToIntFunction<T> intervalFun) {
+        return getContinuousLists(sortedList, value2Int, value -> value, intervalFun);
+    }
+
+    /**
      * 分割出连续数据作为一个新list, 并将原始对象转换成另一个对象
      *
      * @param sortedList 有序原始集合
@@ -73,6 +86,21 @@ public class ContinuousListUtils {
      */
     public static <T, R> List<List<R>> getContinuousLists(List<T> sortedList, ToIntFunction<T> value2Int, Function<T, R> value2Rtn, int interval) {
         return getContinuousLists(sortedList, value2Int, value2Rtn, null, interval);
+    }
+
+    /**
+     * 分割出连续数据作为一个新list, 并将原始对象转换成另一个对象
+     *
+     * @param sortedList  有序原始集合
+     * @param value2Int   将集合中的对象转成int的实现, 做连续判断
+     * @param value2Rtn   原始对象转换成新对象的实现
+     * @param intervalFun 连续间隔
+     * @param <T>         目标值类型(原始值)
+     * @param <R>         返回值类型
+     * @return
+     */
+    public static <T, R> List<List<R>> getContinuousLists(List<T> sortedList, ToIntFunction<T> value2Int, Function<T, R> value2Rtn, ToIntFunction<T> intervalFun) {
+        return getContinuousLists(sortedList, value2Int, value2Rtn, null, intervalFun);
     }
 
 
@@ -89,6 +117,23 @@ public class ContinuousListUtils {
      * @return
      */
     public static <T, R> List<List<R>> getContinuousLists(List<T> list, ToIntFunction<T> value2Int, Function<T, R> value2Rtn, Comparator<T> comparing, int interval) {
+        // 这里的间隔是恒定的, 所以ToIntFunction这个参数直接 传参 anyEle -> interval, 这样就会始终返回interval了
+        return getContinuousLists(list, value2Int, value2Rtn, comparing, anyEle -> interval);
+    }
+
+    /**
+     * 分割出连续数据作为一个新list, 并将原始对象转换成另一个对象
+     *
+     * @param list        原始集合(需要有序或者传一个比较器的实现, 见comparing)
+     * @param value2Int   将集合中的对象转成int的实现, 做连续判断
+     * @param value2Rtn   原始对象转换成新对象的实现
+     * @param comparing   比较器, 用来排序, 如果为空则不排序(这种情况需要保证list有序)
+     * @param intervalFun 连续间隔
+     * @param <T>         目标值类型(原始值)
+     * @param <R>         返回值类型
+     * @return
+     */
+    public static <T, R> List<List<R>> getContinuousLists(List<T> list, ToIntFunction<T> value2Int, Function<T, R> value2Rtn, Comparator<T> comparing, ToIntFunction<T> intervalFun) {
         if (list == null || list.isEmpty()) {
             return new ArrayList<>();
         }
@@ -112,6 +157,8 @@ public class ContinuousListUtils {
             int previousInt = value2Int.applyAsInt(previous);
 
             R returnValue = value2Rtn.apply(current);
+            // 间隔由上一个元素决定
+            int interval = intervalFun.applyAsInt(previous);
 
             if (currentInt == previousInt + interval) {
                 continuousList.add(returnValue);
