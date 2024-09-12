89c53761c5ffdc9fc08c288c57a720ed1c531262
liurl <lylyuanliang@163.com>
2024-09-12 16:35:01 +0800

    feat: 补充异或交换

--- algorithm/src/main/java/com/example/sort/Sort.java
+++ algorithm/src/main/java/com/example/sort/Sort.java
@@ -34,12 +34,46 @@ public interface Sort {
      * @param index2 下标2
      */
     default void exchange(int[] array, int index1, int index2) {
+        swap2(array, index1, index2);
+    }
+
+    /**
+     * 交换数组中的两个元素(index1位置的元素和index2位置的元素交换)
+     *
+     * @param array  数组
+     * @param index1 下标1
+     * @param index2 下标2
+     */
+    default void swap(int[] array, int index1, int index2) {
         if (index1 == index2) {
             return;
         }
         array[index1] = array[index1] + array[index2];
         array[index2] = array[index1] - array[index2];
         array[index1] = array[index1] - array[index2];
+    }
+
+    /**
+     * 交换数组中的两个元素(index1位置的元素和index2位置的元素交换) <br>
+     * 前置知识: <br>
+     * a ^ 0 = a; a ^ a = 0; a ^ b = b ^ a; (a ^ b) ^ c = a ^ (b ^ c); <br>
+     * 所以a,b通过异或交换: <br>
+     * 假设 a = 1, b = 2; <br>
+     * a = a ^ b = 1 ^ 2 <br>
+     * b = a ^ b = (1 ^ 2) ^ 2 = 1 ^ (2 ^ 2) = 1 ^ 0 = 1; <br>
+     * a = a ^ b = (1 ^ 2) ^ 1 = (1 ^ 1) ^ 2 = 0 ^ 2 = 2; <br>
+     *
+     * @param array  数组
+     * @param index1 下标1
+     * @param index2 下标2
+     */
+    default void swap2(int[] array, int index1, int index2) {
+        if (index1 == index2) {
+            return;
+        }
+        array[index1] = array[index1] ^ array[index2];
+        array[index2] = array[index1] ^ array[index2];
+        array[index1] = array[index1] ^ array[index2];
     }
 
     /**
