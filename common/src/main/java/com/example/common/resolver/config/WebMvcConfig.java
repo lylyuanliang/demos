e42b3295b5b8f63842ce30a626d519b65e55b07e
liurl <lylyuanliang@163.com>
2024-10-14 17:12:23 +0800

    fix: 1.添加包扫描. 2.修改模块名

--- common/src/main/java/com/example/common/resolver/config/WebMvcConfig.java
+++ common/src/main/java/com/example/common/resolver/config/WebMvcConfig.java
@@ -3,15 +3,14 @@ package com.example.common.resolver.config;
 import com.example.common.resolver.CustomRequestBodyResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @Configuration
-public class WebMvcConfig extends WebMvcConfigurationSupport {
+public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-        super.addArgumentResolvers(resolvers);
         resolvers.add(new CustomRequestBodyResolver());
     }
 }
--- service/src/main/java/com/example/service/ServiceApplication.java
+++ service/src/main/java/com/example/service/ServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.example"})
 public class ServiceApplication {
 
 	public static void main(String[] args) {
