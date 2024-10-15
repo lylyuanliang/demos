682c2e5ce26addcfcd903fb2f5d44f11134e87d1
liurl <lylyuanliang@163.com>
2024-10-15 17:11:04 +0800

    fix: 1.使用lombok

--- service/src/main/java/com/example/service/cases/resolverdemo/Param1.java
+++ service/src/main/java/com/example/service/cases/resolverdemo/Param1.java
@@ -1,25 +1,14 @@
 package com.example.service.cases.resolverdemo;
 
 import com.example.common.resolver.annotation.SubClassAnnotation;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
+
+@EqualsAndHashCode(callSuper = true)
 @SubClassAnnotation(type = "type1")
+@Data
 public class Param1 extends ParamBase{
     private String param1Value;
     private String param1Value1;
-
-    public String getParam1Value() {
-        return param1Value;
-    }
-
-    public void setParam1Value(String param1Value) {
-        this.param1Value = param1Value;
-    }
-
-    public String getParam1Value1() {
-        return param1Value1;
-    }
-
-    public void setParam1Value1(String param1Value1) {
-        this.param1Value1 = param1Value1;
-    }
 }
--- service/src/main/java/com/example/service/cases/resolverdemo/Param2.java
+++ service/src/main/java/com/example/service/cases/resolverdemo/Param2.java
@@ -1,25 +1,13 @@
 package com.example.service.cases.resolverdemo;
 
 import com.example.common.resolver.annotation.SubClassAnnotation;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
+@EqualsAndHashCode(callSuper = true)
 @SubClassAnnotation(type = "type2")
+@Data
 public class Param2 extends ParamBase{
     private String param2Value;
     private Integer param2Value2;
-
-    public String getParam2Value() {
-        return param2Value;
-    }
-
-    public void setParam2Value(String param2Value) {
-        this.param2Value = param2Value;
-    }
-
-    public Integer getParam2Value2() {
-        return param2Value2;
-    }
-
-    public void setParam2Value2(Integer param2Value2) {
-        this.param2Value2 = param2Value2;
-    }
 }
